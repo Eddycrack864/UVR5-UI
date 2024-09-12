f064d5801707fe3f908007bc0d1f4d0cbadaefb5
arisdev <preciadojulian849@gmail.com>
2024-09-12 07:38:28 -0600

    test

--- app.py
+++ app.py
@@ -935,9 +935,34 @@ with gr.Blocks(theme="NoCrypt/miku@1.2.2", title="🎵 UVR5 UI 🎵") as app:
                 )
             
             demucs_button.click(demucs_separator, [demucs_audio, demucs_model, demucs_output_format, demucs_shifts, demucs_overlap], [demucs_stem1, demucs_stem2, demucs_stem3, demucs_stem4])
+            
+            from assets.i18n.i18n import I18nAuto
 
-            with gr.Tab(i18n("Settings")):
-                    select_themes_tab()
+            import assets.themes.loadThemes as loadThemes
+
+            i18n = I18nAuto()
+
+        with gr.TabItem("Settings"):
+            now_dir = os.getcwd()
+        sys.path.append(now_dir)
+
+        def select_themes_tab():
+            with gr.Column():
+                themes_select = gr.Dropdown(
+            choices=loadThemes.get_list(),
+            value=loadThemes.read_json(),
+            label=i18n("Theme"),
+            info=i18n(
+                "Select the theme you want to use. (Requires restarting the App)"
+            ),
+            visible=True,
+        )
+        themes_select.change(
+            fn=loadThemes.select_theme,
+            inputs=themes_select,
+            outputs=[],
+        )
+
 
         with gr.TabItem("Credits"):
            gr.Markdown(

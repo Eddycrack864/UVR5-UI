72acfed9d7859249d947562674e7ff0c8c209757
Eddycrack 864 <89285504+Eddycrack864@users.noreply.github.com>
2024-11-10 00:13:02 -0500

    fix for pytorch

--- UVR5-UI-installer.bat
+++ UVR5-UI-installer.bat
@@ -80,6 +80,8 @@ exit /b 0
 echo Installing dependencies...
 call "%MINICONDA_DIR%\condabin\conda.bat" activate "%ENV_DIR%" || goto :error
 pip install -r "%INSTALL_DIR%\requirements.txt" || goto :error
+pip uninstall torch torchvision -y || goto :error
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cu121 || goto :error
 call "%MINICONDA_DIR%\condabin\conda.bat" deactivate
 echo Dependencies installation complete
 echo.
